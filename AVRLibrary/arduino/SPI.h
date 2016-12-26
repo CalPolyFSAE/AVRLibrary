5103826bd8e60cc06bea80bd4f9178b071a5d8bd
Nick Mah <nickjmah@gmail.com>
2016-12-25 22:59:56 -0800

    Made SPI header since it was missing. Included 16b and 8b transmission and a generic SPI initialize function

---
+++ AVRLibrary/arduino/SPI.h
@@ -0,0 +1,46 @@
+/** SPI Library
+ * Nick Mah
+ * Library contains various SPI functions to be used
+ */
+#include <stdio.h>
+#include <avr/io.h>
+
+uint16_t Transmit_SPI_Master_16(uint8_t Data1, uint8_t Data2) //Function for ATMega to write data to. Must be rewritten to read data from ADC
+{
+	char receive;
+	PORTB &= ~(1 << CS0);			// assert the slave select
+	SPDR = Data1;     			// Start transmission, send high byte first
+	while (!(SPSR & (1<<SPIF)));// Wait (poll) for transmission complete
+	receive = SPDR;	//save first return
+	SPDR = Data2;		//start second transmission
+	while(!(SPSR & (1 <<SPIF)));	//wait for transmission complete
+	PORTB |= 1 << CS0;			// deassert the slave select
+	return (receive << 8) | SPDR; //return first return concatenated with second return
+}
+
+uint16_t Transmit_SPI_Master(uint8_t Data) //Function for ATMega to write data to. Must be rewritten to read data from ADC
+{
+	char receive;
+	PORTB &= ~(1 << CS0);			// assert the slave select
+	SPDR = Data;     			// Start transmission, send high byte first
+	while (!(SPSR & (1<<SPIF)));// Wait (poll) for transmission complete
+	receive = SPDR;	//save first return
+	PORTB |= 1 << CS0;			// deassert the slave select
+	return (receive << 8) | SPDR; //return first return concatenated with second return
+}
+
+void Initialize_SPI_Master(void) //correct values for each register still need to be determined
+		{
+	SPCR = (0 << SPIE) | 		//No interrupts
+			(1 << SPE) | 				//SPI enabled
+			(0 << DORD) | 			//send MSB first
+			(1 << MSTR) | 			//master
+			(0 << CPOL) | 			//clock idles low
+			(0 << CPHA) | 			//sample leading edge
+			(0 << SPR1) | (0 << SPR0); //clock speed
+	SPSR = (0 << SPIF) | 		//SPI interrupt flag
+			(0 << WCOL) | 			//Write collision flag
+			(0 << SPI2X); 			//Doubles SPI clock
+	PORTB = 1 << CS0;  		// make sure SS is high
+
+}
--- AVRLibrary/arduino/float16.hpp
+++ AVRLibrary/arduino/float16.hpp
@@ -43,28 +43,29 @@ public:
 //  - Does not support infinities or NaN
 //  - Few, partially pipelinable, non-branching instructions,
 //  - Core opreations ~10 clock cycles on modern x86-64
-static void float16(uint16_t* out, const float in) {
-   uint32_t inu = *((uint32_t*) &in);
-   uint32_t t1;
-   uint32_t t2;
-   uint32_t t3;
 
-   t1 = inu & 0x7fffffff;                 // Non-sign bits
-   t2 = inu & 0x80000000;                 // Sign bit
-   t3 = inu & 0x7f800000;                 // Exponent
+	static void float16(uint16_t* out, const float in) {
+	   uint32_t inu = *((uint32_t*) &in);
+	   uint32_t t1;
+	   uint32_t t2;
+	   uint32_t t3;
 
-   t1 >>= 13;                             // Align mantissa on MSB
-   t2 >>= 16;                             // Shift sign bit into position
+	   t1 = inu & 0x7fffffff;                 // Non-sign bits
+	   t2 = inu & 0x80000000;                 // Sign bit
+	   t3 = inu & 0x7f800000;                 // Exponent
 
-   t1 -= 0x1c000;                         // Adjust bias
+	   t1 >>= 13;                             // Align mantissa on MSB
+	   t2 >>= 16;                             // Shift sign bit into position
 
-   t1 = (t3 > 0x38800000) ? 0 : t1;       // Flush-to-zero
-   t1 = (t3 < 0x8e000000) ? 0x7bff : t1;  // Clamp-to-max
-   t1 = (t3 == 0 ? 0 : t1);               // Denormals-as-zero
+	   t1 -= 0x1c000;                         // Adjust bias
 
-   t1 |= t2;                              // Re-insert sign bit
+	   t1 = (t3 > 0x38800000) ? 0 : t1;       // Flush-to-zero
+	   t1 = (t3 < 0x8e000000) ? 0x7bff : t1;  // Clamp-to-max
+	   t1 = (t3 == 0 ? 0 : t1);               // Denormals-as-zero
 
-   *((uint16_t*) out) = t1;
-}
+	   t1 |= t2;                              // Re-insert sign bit
+
+	   *((uint16_t*) out) = t1;
+	}
 };
 #endif
