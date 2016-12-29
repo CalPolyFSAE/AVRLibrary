0fdf4984db1f56c2ecf6fb011e5eb4235dcb7451
Nick Mah <nickjmah@gmail.com>
2016-12-28 16:42:29 -0800

    changed SPI transmit functions to ignore interrupts

--- AVRLibrary/arduino/SPI.h
+++ AVRLibrary/arduino/SPI.h
@@ -4,9 +4,13 @@
  */
 #include <stdio.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 uint16_t Transmit_SPI_Master_16(uint8_t Data1, uint8_t Data2, uint8_t CS) //Function for ATMega to write data to. Must be rewritten to read data from ADC
 {
+	uint8_t sreg;
+	sreg = SREG; //save current global interrupt state
+	cli(); //disable interrupts
 	char receive;
 	PORTB &= ~(1 << CS);			// assert the slave select
 	SPDR = Data1;     			// Start transmission, send high byte first
@@ -15,18 +19,23 @@ uint16_t Transmit_SPI_Master_16(uint8_t Data1, uint8_t Data2, uint8_t CS) //Func
 	SPDR = Data2;		//start second transmission
 	while(!(SPSR & (1 <<SPIF)));	//wait for transmission complete
 	PORTB |= 1 << CS;			// deassert the slave select
+	SREG = sreg; //restore global interrupt state
 	return (receive << 8) | SPDR; //return first return concatenated with second return
 }
 
-uint16_t Transmit_SPI_Master(uint8_t Data, uint8_t CS) //Function for ATMega to write data to. Must be rewritten to read data from ADC
+uint8_t Transmit_SPI_Master(uint8_t Data, uint8_t CS) //Function for ATMega to write data to. Must be rewritten to read data from ADC
 {
-	char receive;
+	uint8_t sreg;
+	sreg = SREG; //see above function
+	cli();
+	uint8_t receive;
 	PORTB &= ~(1 << CS);			// assert the slave select
 	SPDR = Data;     			// Start transmission, send high byte first
 	while (!(SPSR & (1<<SPIF)));// Wait (poll) for transmission complete
 	receive = SPDR;	//save first return
 	PORTB |= 1 << CS;			// deassert the slave select
-	return (receive << 8) | SPDR; //return first return concatenated with second return
+	SREG = sreg;
+	return receive; //return first return concatenated with second return
 }
 
 void Initialize_SPI_Master(uint8_t CS) //correct values for each register still need to be determined
